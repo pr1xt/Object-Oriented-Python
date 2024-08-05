3ae2c4648d2a916a7c733406d3ed11d31535b827
Timur <93942023+pr1xt@users.noreply.github.com>
2024-08-05 21:50:38 +0200

    v3

---
+++ chapter4/accounts.py
@@ -0,0 +1,25 @@
+from bank import  bank
+
+accounts = {}
+counter = 0
+
+newAcc = bank("Joe",100,"3546")
+accounts[f"{counter}"] = newAcc
+counter += 1
+
+newAcc = bank("Marry",60,"oioi")
+accounts[f"{counter}"] = newAcc
+counter += 1
+
+newAcc = bank("Henry",15,"35g0")
+accounts[f"{counter}"] = newAcc
+counter += 1
+
+newAcc = bank("Beata",99,"5302")
+accounts[f"{counter}"] = newAcc
+counter += 1
+
+newAcc = bank("Mark",180,"face")
+accounts[f"{counter}"] = newAcc
+counter += 1
+
